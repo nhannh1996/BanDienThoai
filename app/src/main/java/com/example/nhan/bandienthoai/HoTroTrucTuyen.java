af94b5c1f4edf574f3f906c4a787a7a282fbf2a1
nhannh1996 <nhannh1996@gmail.com>
2017-04-19 13:53:08 +0700

    first commit

---
+++ app/src/main/java/com/example/nhan/bandienthoai/HoTroTrucTuyen.java
@@ -0,0 +1,13 @@
+package com.example.nhan.bandienthoai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HoTroTrucTuyen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ho_tro_truc_tuyen);
+    }
+}
